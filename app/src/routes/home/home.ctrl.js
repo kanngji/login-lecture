6ec25e04f5ddfd8c416e516a31d3be65134eabee
kanngji <alleneffort1994@gmail.com>
2022-05-04 00:54:19 +0900

    프론트 로그인

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,40 @@
 "use strict";
-const home = (req, res) => {
-  res.render("home/index");
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
+};
+const users = {
+  id: ["jihoon", "김현수", "오준표"],
+  psword: ["1234", "12345", "123456"],
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다",
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
